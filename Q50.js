08c0894f502543a63a81a3d3d370d752e9e4ad76
Shaman Baloch <shamanbalouch07@gmail.com>
2024-03-07 23:53:27 +0500

    Add files via upload
    increment and decrement

---
+++ Q50.js
@@ -0,0 +1,6 @@
+//increment and dicreament in typescript 
+var n = 5;
+var m = 0;
+var c = n-- + ++n + n++ + ++m + m++ + n-- + --m;
+//    5 +  5  +  5  +    1   +  1 +  6  +   1  
+console.log(c);
---
+++ Q50.ts
@@ -0,0 +1,27 @@
+
+
+//predicreament in typescript 
+let n=5
+let m=0
+
+console.log(n--)  // first we print value then we dicreament like n value here 5 , 
+//then here 5 will be output because first print value then it dicreament 
+
+console.log(n) /// now here dicreamint value will bi print 4
+
+console.log(--n)  // this call post dicrement , first substract value one then print , 
+//like befor this line above n value is 4 after this n value will be change  thst is 3
+
+
+//increment and dicreament in typescript 
+//let n=5
+// let m=0
+
+
+
+// let c=++n + n++ + m++ + m++ + n-- +  m-- ;
+//   //    6 +  6  +  0  + 1   + 7   +  2      
+
+// console.log(c)
+
+  
